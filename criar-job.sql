8ad7d381aa917a87dc0171d919d955f1a374bf5c
Valmir <vmcfilho@gmail.com>
2024-06-05 11:00:06 -0300

    Create criar-job.sql

---
+++ criar-job.sql
@@ -0,0 +1,12 @@
+DECLARE x integer; 
+BEGIN
+      dbms_job.submit (x, 'ATUALIZAR_LOC_EQUIP_ADIC;', to_date('05-06-2024 11:00:00', 'dd-mm-yyyy hh24:mi:ss'),'SYSDATE + 1/24/60*5');
+      COMMIT;
+END;
+
+/*
+O primeiro parâmetro não deve ser preenchido;
+O segundo é o nome da procedure ou comando SQL a ser executado;
+O terceiro é a data de início da execução (primeira execução);
+O último dita a peridiciodade de execução. No exemplo acima: a cada dia, a uma hora da manhã;
+*/
