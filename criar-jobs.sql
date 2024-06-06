74ddf63827f8bca32098f5a3128dac0623e828c2
Valmir <vmcfilho@gmail.com>
2024-06-06 19:40:47 -0300

    Update and rename criar-job.sql to criar-jobs.sql

--- criar-jobs.sql
+++ criar-jobs.sql
@@ -4,6 +4,14 @@ BEGIN
       COMMIT;
 END;
 
+/
+DECLARE x integer; 
+BEGIN
+      dbms_job.submit (x, 'UNIFICA_OS_LOGINS(''VAFILHO'');', to_date('05-06-2024 11:00:00', 'dd-mm-yyyy hh24:mi:ss'),'SYSDATE + 1/24/60*5');
+      COMMIT;
+END;
+/
+
 /*
 O primeiro parâmetro não deve ser preenchido;
 O segundo é o nome da procedure ou comando SQL a ser executado;
